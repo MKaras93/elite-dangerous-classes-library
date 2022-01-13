0f13e7c8c3a05c5bd0a52f1d255531f3c1e2d945
Michal Karas <miczipl@gmail.com>
2022-01-13 16:20:58 +0000

    build package

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
-import pathlib
-from setuptools import setup, find_packages
-
-HERE = pathlib.Path(__file__).parent
-
-README = (HERE / "README.md").read_text()
+from setuptools import setup
 
 setup(
     name="elite-dangerous-classes-library",
-    version="1.0.0",
-    url="https://github.com/MKaras93/elite-dangerous-classes-library",
-    author="Michal Karas",
-    packages=find_packages(exclude=("tests",)),
+    version="0.0.1",
+    packages=["elite-dangerous-classes-library"],
 )
