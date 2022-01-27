3994b2f828d571dc83984abb555d5bc3c221710c
Michal Karas <miczipl@gmail.com>
2022-01-27 21:13:48 +0000

    add basic factories and example test for them

--- tests/factories/classes_factories.py
+++ tests/factories/classes_factories.py
@@ -1,9 +1,40 @@
 import factory
-from edclasses import System
+from edclasses import System, Faction, FactionBranch, OrbitalStation
+from edclasses.enums import StationType
 
 
 class SystemFactory(factory.Factory):
     class Meta:
         model = System
 
-    name = "system_name"
+    name = "System"
+
+
+class FactionFactory(factory.Factory):
+    class Meta:
+        model = Faction
+
+    name = "Faction"
+
+
+class FactionBranchFactory(factory.Factory):
+    class Meta:
+        model = FactionBranch
+
+    faction = factory.SubFactory(FactionFactory)
+    system = factory.SubFactory(SystemFactory)
+    is_main = False  # we don't need to know faction's main branch - usually we won't have this information
+    influence = 50
+    stations = list()
+
+
+class OrbitalStationFactory(factory.Factory):
+    class Meta:
+        model = OrbitalStation
+
+    name = "Orbital Station"
+    station_type = StationType.CORIOLIS_STARPORT
+    system = factory.SubFactory(SystemFactory)
+    distance_to_arrival = 100
+    services = list()
+    controlling_faction = factory.SubFactory(FactionBranchFactory)
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,7 +1,15 @@
-from tests.factories.classes_factories import SystemFactory
+from tests.factories.classes_factories import SystemFactory, FactionFactory, FactionBranchFactory, OrbitalStationFactory
 
 
 def test_system_factory():
     # TODO: remove, it's an exmaple test to check pytest and factory setup
     system = SystemFactory()
-    assert system.name == "system_name"
+    assert system.name == "System"
+
+
+def test_classes():
+    # TODO: remove, it's an example test to check pytest and factory setup
+    system = SystemFactory()
+    faction = FactionFactory()
+    faction_branch = FactionBranchFactory()
+    station = OrbitalStationFactory()
