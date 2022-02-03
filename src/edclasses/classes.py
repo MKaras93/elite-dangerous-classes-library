6cf138e2d6d6685178640bd1b6f856dd1f65fd3a
Michal Karas <miczipl@gmail.com>
2022-02-03 23:20:14 +0000

    add UniqueInstanceMixin

--- src/edclasses/classes.py
+++ src/edclasses/classes.py
@@ -5,9 +5,41 @@ import edclasses.api_adapters.elite_bgs_adapter as bgs_adapter
 from . import enums
 
 
-class System:
+class UniqueInstanceMixin:
+    registry = {}
+    keys = tuple()
+
+    @classmethod
+    def create(cls, **kwargs):
+        try:
+            return cls(**kwargs)
+        except ValueError:
+            return cls.get_from_registry(**kwargs)
+
+    @classmethod
+    def _get_key(cls, *args, **kwargs):
+        return tuple(kwargs[attr] for attr in cls.keys)
+
+    @classmethod
+    def get_from_registry(cls, **kwargs):
+        obj_key = cls._get_key(**kwargs)
+        return cls.registry.get(obj_key)
+
+    def __init__(self, *args, **kwargs):
+        obj_key = self._get_key(**self.__dict__)
+        obj = self.__class__.registry.get(obj_key)
+        if obj is not None:
+            raise ValueError
+        self.__class__.registry[obj_key] = self
+        super().__init__()
+
+
+class System(UniqueInstanceMixin):
+    keys = ("name",)
+
     def __init__(self, name: str):
         self.name = name
+        super().__init__()
 
     def __repr__(self):
         return f"System '{self.name}'"
