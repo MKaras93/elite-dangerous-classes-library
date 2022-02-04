5dda8a4b20c94138bc56c26739bed69f9acd1632
Michal Karas <miczipl@gmail.com>
2022-02-04 00:33:24 +0000

    set up properties, fix some bugs

--- src/edclasses/classes.py
+++ src/edclasses/classes.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from typing import Optional, List
+from typing import Optional, List, Set
 
 import edclasses.api_adapters.elite_bgs_adapter as bgs_adapter
 from . import enums
@@ -8,6 +8,7 @@ from .utils import UniqueInstanceMixin
 
 class System(UniqueInstanceMixin):
     keys = ("name",)
+    registry = {}
 
     def __init__(self, name: str):
         self.name = name
@@ -18,9 +19,32 @@ class System(UniqueInstanceMixin):
     def __repr__(self):
         return f"System '{self.name}'"
 
+    def add_faction_branch(self, faction: "Faction") -> "FactionBranch":
+        faction_branch = FactionBranch.create(system=self, faction=faction)
+        self._faction_branches.add(faction_branch)
+        faction._faction_branches.add(faction_branch)
+        return faction_branch
+
+    def add_station(self, **kwargs) -> "OrbitalStation":
+        station = OrbitalStation.create(system=self, **kwargs)
+        self._stations.add(station)
+        return station
+        # controlling_faction_branch = kwargs.get("controlling_faction")
+        # if controlling_faction_branch is not None:
+        #     controlling_faction_branch._stations.add(station)
+
+    @property
+    def stations(self) -> Set["OrbitalStation"]:
+        return self._stations
+
+    @property
+    def faction_branches(self) -> Set["FactionBranch"]:
+        return self._faction_branches
+
 
 class Faction(UniqueInstanceMixin):
     keys = ("name",)
+    registry = {}
 
     def __init__(self, name: str):
         self.name = name
@@ -30,12 +54,17 @@ class Faction(UniqueInstanceMixin):
     def __repr__(self):
         return f"Faction '{self.name}'"
 
+    @property
+    def faction_branches(self) -> Set["FactionBranch"]:
+        return self._faction_branches
+
 
 class FactionBranch(UniqueInstanceMixin):
     keys = (
         "faction",
         "system",
     )
+    registry = {}
     adapter = bgs_adapter.EliteBgsFactionBranchAdapter()
 
     def __init__(
@@ -55,12 +84,33 @@ class FactionBranch(UniqueInstanceMixin):
     def __repr__(self):
         return f"{self._faction} in {self._system}"
 
+    def delete(self):
+        try:
+            self._faction._faction_branches.remove(self)
+            self._system._faction_branches.remove(self)
+            del self
+        except KeyError:
+            super().delete()
+
+    @property
+    def faction(self) -> Faction:
+        return self._faction
+
+    @property
+    def system(self) -> System:
+        return self._system
+
+    @property
+    def stations(self) -> Set["OrbitalStation"]:
+        return self._stations
+
 
 class OrbitalStation(UniqueInstanceMixin):
     keys = (
         "name",
         "system",
     )
+    registry = {}
 
     def __init__(
         self,
@@ -72,7 +122,7 @@ class OrbitalStation(UniqueInstanceMixin):
     ):
         self.name = name
         self.station_type = station_type.value
-        self.system = system
+        self._system = system
         self.distance_to_arrival = distance_to_arrival
         self.services = services or []
         self._controlling_faction = None
@@ -80,3 +130,22 @@ class OrbitalStation(UniqueInstanceMixin):
 
     def __repr__(self):
         return f"{self.station_type.title()} '{self.name}'"
+
+    @property
+    def controlling_faction(self) -> FactionBranch:
+        return self._controlling_faction
+
+    @controlling_faction.setter
+    def controlling_faction(self, faction_branch: FactionBranch):
+        if faction_branch is self._controlling_faction:
+            return
+
+        if self._controlling_faction and self._controlling_faction is not faction_branch:
+            self._controlling_faction._stations.remove(self)
+            if faction_branch is not None:
+                faction_branch._stations.add(self)
+        self._controlling_faction = faction_branch
+
+    @property
+    def system(self) -> System:
+        return self._system
--- src/edclasses/utils.py
+++ src/edclasses/utils.py
@@ -15,7 +15,11 @@ class UniqueInstanceMixin:
 
     @classmethod
     def _get_key(cls, *args, **kwargs):
-        return tuple(kwargs[attr] for attr in cls.keys)
+        if args:
+            instance = args[0]
+            return tuple(getattr(instance,attr) for attr in cls.keys)
+        else:
+            return tuple(kwargs[attr] for attr in cls.keys)
 
     @classmethod
     def get_from_registry(cls, **kwargs):
@@ -23,9 +27,16 @@ class UniqueInstanceMixin:
         return cls.registry.get(obj_key)
 
     def __init__(self, *args, **kwargs):
-        obj_key = self._get_key(**self.__dict__)
+        obj_key = self._get_key(self)
         obj = self.__class__.registry.get(obj_key)
         if obj is not None:
             raise ValueError
         self.__class__.registry[obj_key] = self
         super().__init__()
+
+    def remove_from_registry(self):
+        obj_key = self._get_key(**self.__dict__)
+        self.registry.pop(obj_key)
+
+    def delete(self):
+        self.remove_from_registry()
