acd1c92535127cd324d9dbf81081eecca9b649e4
Michal Karas <miczipl@gmail.com>
2022-02-16 21:06:31 +0000

    add OneToManyRelation tests; remove dirty edclasses tests

--- src/edclasses/tests/test_classes.py
+++ src/edclasses/tests/test_classes.py
@@ -1,67 +1,6 @@
-from .factories.classes_factories import (
-    SystemFactory,
-    FactionFactory,
-    FactionBranchFactory,
-    OrbitalStationFactory,
-)
-from .. import System, Faction, enums, FactionBranch
+from .factories.classes_factories import *
 
 
 def test_system_factory():
-    # TODO: remove, it's an exmaple test to check pytest and factory setup
     system = SystemFactory()
     assert system.name == "System 0"
-
-
-def test_classes():
-    # TODO: remove, it's an example test to check pytest and factory setup
-    system = SystemFactory()
-    faction = FactionFactory()
-    faction_branch = FactionBranchFactory()
-    station = OrbitalStationFactory()
-
-
-class TestSystem:
-    class TestCreateMethods:
-        def test_create_branch_updates_related_objects(self):
-            system: System = SystemFactory()
-            faction: Faction = FactionFactory()
-            faction2: Faction = FactionFactory()
-
-            faction_branch_1 = FactionBranchFactory(faction=faction, system=system)
-            faction_branch_2 = FactionBranchFactory(faction=faction2, system=system)
-
-            assert faction_branch_1.system == system
-            assert faction_branch_1.faction == faction
-            assert faction_branch_2.system == system
-            assert faction_branch_2.faction == faction2
-            assert system.faction_branches == [faction_branch_1, faction_branch_2]
-            assert faction.faction_branches == [faction_branch_1]
-            assert faction2.faction_branches == [faction_branch_2]
-
-        def test_create_station_updates_related_objects(self):
-            system: System = SystemFactory()
-            faction_branch = FactionBranchFactory(system=system)
-
-            station1 = OrbitalStationFactory(station_type=enums.StationType.STATION, controlling_faction=faction_branch, system=system)
-            station2 = OrbitalStationFactory(station_type=enums.StationType.STATION, controlling_faction=faction_branch, system=system)
-
-            assert station1.station_type == enums.StationType.STATION.value
-            assert station1.system == system
-            assert station1.controlling_faction == faction_branch
-            assert station2.system == system
-            assert station2.controlling_faction == faction_branch
-            assert system.stations == [station1, station2]
-            assert faction_branch.stations == [station1, station2]
-
-
-class TestLinks:
-    def test_links(self):
-        system = SystemFactory()
-        faction = FactionFactory(name="faction1")
-        faction2 = FactionFactory(name="faction2")
-        faction_branch = FactionBranchFactory(faction=faction, system=system)
-        faction_branch2 = FactionBranchFactory(faction=faction2, system=system)
-        assert system.faction_branches == [faction_branch, faction_branch2]
-        assert faction.faction_branches == [faction_branch]
-        assert faction2.faction_branches == [faction_branch2]
---
+++ src/edclasses/tests/test_relations.py
@@ -0,0 +1,108 @@
+from edclasses import OneToManyRelation
+
+
+class Parent:
+    _children_relation = OneToManyRelation.create(
+        parent_class_name="Parent", child_class_name="Child"
+    )
+
+    def __init__(self, children=None):
+        self.children = children or []
+
+    def _children_setter(self, value):
+        self._children_relation.set_for_parent(self, value)
+
+    def _children_getter(self):
+        return self._children_relation.get_for_parent(self)
+
+    children = property(fget=_children_getter, fset=_children_setter)
+
+
+class Child:
+    _parent_relation = OneToManyRelation.create(
+        parent_class_name="Parent", child_class_name="Child"
+    )
+
+    def __init__(self, parent=None):
+        self.parent = parent
+
+    def _parent_setter(self, value):
+        self._parent_relation.set_for_child(self, value)
+
+    def _parent_getter(self):
+        return self._parent_relation.get_for_child(self)
+
+    parent = property(fget=_parent_getter, fset=_parent_setter)
+
+
+class TestOneToManyRelation:
+    class TestChildSide:
+        def test_removing_parent_removes_link(self):
+            parent = Parent()
+            child = Child(parent=parent)
+            assert child.parent == parent
+            assert parent.children == [child]
+
+            child.parent = None
+
+            assert child.parent is None
+            assert parent.children == []
+
+        def test_adding_parent_creates_link(self):
+            parent = Parent()
+            child = Child()
+
+            child.parent = parent
+
+            assert child.parent is parent
+            assert parent.children == [child]
+
+        def test_replacing_parent_replaces_link(self):
+            old_parent = Parent()
+            new_parent = Parent()
+            child = Child(parent=old_parent)
+            assert old_parent is not new_parent
+            assert child.parent is old_parent
+            assert old_parent.children == [child]
+
+            child.parent = new_parent
+
+            assert child.parent is new_parent
+            assert new_parent.children == [child]
+            assert old_parent.children == []
+
+    class TestParentSide:
+        def test_removing_children_removes_links(self):
+            child1 = Child()
+            child2 = Child()
+            parent = Parent(children=[child1, child2])
+            assert child1.parent is parent
+            assert child2.parent is parent
+
+            parent.children = []
+
+            assert child1.parent is None
+            assert child2.parent is None
+
+        def test_adding_children_adds_links(self):
+            child1 = Child()
+            child2 = Child()
+            parent = Parent(children=[])
+
+            parent.children = [child1, child2]
+
+            assert child1.parent is parent
+            assert child2.parent is parent
+
+        def test_stealing_children_replaces_links(self):
+            old_parent = Parent()
+            child1 = Child(parent=old_parent)
+            child2 = Child(parent=old_parent)
+            new_parent = Parent()
+            assert old_parent.children == [child1, child2]
+            new_parent.children = [child2]
+
+            assert child1.parent is old_parent
+            assert child2.parent is new_parent
+            assert new_parent.children == [child2]
+            assert old_parent.children == [child1]
