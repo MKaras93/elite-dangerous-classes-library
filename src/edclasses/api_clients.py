d21f3290659da59bd9a8831ebd2b055d4854356b
Michal Karas <miczipl@gmail.com>
2022-02-17 22:52:48 +0000

    add lru_cache to EliteBgsClient.get_request

--- src/edclasses/api_clients.py
+++ src/edclasses/api_clients.py
@@ -1,11 +1,16 @@
 from urllib import parse
 
+
 import requests
+from functools import lru_cache
 
 
 class EliteBgsClient:
     API_URL = "https://elitebgs.app/api/ebgs/v5/"
 
+    @lru_cache  # TODO: replace this with a proper cache:
+    # we should have a time expiring cache, would be best if we would save single objects from API response
+    # (e.g. object representing faction presence) and then reuse that.
     def get_request(self, path="", **kwargs):
         url = parse.urljoin(self.API_URL, path)
         response = requests.get(url, params=kwargs)
