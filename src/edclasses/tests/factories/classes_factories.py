7e09b681bcc0b5e078b11a874e8f9797ff1e9ebb
Michał Karaś <mjkaras93@gmail.com>
2022-09-13 23:12:28 +0200

    fix factories names

--- src/edclasses/tests/factories/classes_factories.py
+++ src/edclasses/tests/factories/classes_factories.py
@@ -11,38 +11,38 @@ class EDModelFactoryBase(factory.Factory):
     adapter = MOCKED_ADAPTER
 
 
-class SystemFactoryBase(EDModelFactoryBase):
+class SystemFactory(EDModelFactoryBase):
     class Meta:
         model = System
 
     name = factory.Sequence(lambda n: f"System {n}")
 
 
-class FactionFactoryBase(EDModelFactoryBase):
+class FactionFactory(EDModelFactoryBase):
     class Meta:
         model = Faction
 
     name = factory.Sequence(lambda n: f"Faction {n}")
 
 
-class FactionBranchFactoryBase(EDModelFactoryBase):
+class FactionBranchFactory(EDModelFactoryBase):
     class Meta:
         model = FactionBranch
 
-    faction = factory.SubFactory(FactionFactoryBase)
-    system = factory.SubFactory(SystemFactoryBase)
+    faction = factory.SubFactory(FactionFactory)
+    system = factory.SubFactory(SystemFactory)
     stations = list()
     is_main = False  # we don't need to know faction's main branch - usually we won't have this information
     influence = 50
 
 
-class OrbitalStationFactoryBase(EDModelFactoryBase):
+class OrbitalStationFactory(EDModelFactoryBase):
     class Meta:
         model = OrbitalStation
 
     name = factory.Sequence(lambda n: f"Orbital Station {n}")
     station_type = StationType.STATION
-    system = factory.SubFactory(SystemFactoryBase)
+    system = factory.SubFactory(SystemFactory)
     distance_to_arrival = 100
     services = list()
-    controlling_faction = factory.SubFactory(FactionBranchFactoryBase)
+    controlling_faction = factory.SubFactory(FactionBranchFactory)
--- src/edclasses/tests/test_classes.py
+++ src/edclasses/tests/test_classes.py
@@ -2,20 +2,20 @@ from .factories.classes_factories import *
 
 
 def test_system_factory():
-    system = SystemFactoryBase()
+    system = SystemFactory()
     assert system.name == "System 0"
 
 
 def test_station_system_factory():
-    station = OrbitalStationFactoryBase()
+    station = OrbitalStationFactory()
     system = station.system
     assert station.system == system
     assert system.stations == [station]
 
 
 def test_station_system_factory_2():
-    system = SystemFactoryBase()
-    station = OrbitalStationFactoryBase(system=system)
+    system = SystemFactory()
+    station = OrbitalStationFactory(system=system)
     assert station.system == system
     assert system.stations == [station]
 
@@ -30,9 +30,9 @@ def test_classes():
 
 
 def test_faction_branches_stations():
-    faction_branch = FactionBranchFactoryBase()
+    faction_branch = FactionBranchFactory()
     system = faction_branch.system
-    orbital_station = OrbitalStationFactoryBase(system=system, controlling_faction=faction_branch)
+    orbital_station = OrbitalStationFactory(system=system, controlling_faction=faction_branch)
 
     assert faction_branch.system is system
     assert orbital_station.system is system
