3d3a640b6935b0323bc0a7447da4bf368d335f78
Michał Karaś <mjkaras93@gmail.com>
2022-09-13 23:05:19 +0200

    force factories to use MockedAdapter

--- src/edclasses/tests/factories/classes_factories.py
+++ src/edclasses/tests/factories/classes_factories.py
@@ -1,41 +1,48 @@
 import factory
 
+from ..mocked_adapter import MockedAdapter
 from ...classes import System, Faction, FactionBranch, OrbitalStation
 from ...enums import StationType
 
+MOCKED_ADAPTER = MockedAdapter()
 
-class SystemFactory(factory.Factory):
+
+class EDModelFactoryBase(factory.Factory):
+    adapter = MOCKED_ADAPTER
+
+
+class SystemFactoryBase(EDModelFactoryBase):
     class Meta:
         model = System
 
     name = factory.Sequence(lambda n: f"System {n}")
 
 
-class FactionFactory(factory.Factory):
+class FactionFactoryBase(EDModelFactoryBase):
     class Meta:
         model = Faction
 
     name = factory.Sequence(lambda n: f"Faction {n}")
 
 
-class FactionBranchFactory(factory.Factory):
+class FactionBranchFactoryBase(EDModelFactoryBase):
     class Meta:
         model = FactionBranch
 
-    faction = factory.SubFactory(FactionFactory)
-    system = factory.SubFactory(SystemFactory)
+    faction = factory.SubFactory(FactionFactoryBase)
+    system = factory.SubFactory(SystemFactoryBase)
     stations = list()
     is_main = False  # we don't need to know faction's main branch - usually we won't have this information
     influence = 50
 
 
-class OrbitalStationFactory(factory.Factory):
+class OrbitalStationFactoryBase(EDModelFactoryBase):
     class Meta:
         model = OrbitalStation
 
     name = factory.Sequence(lambda n: f"Orbital Station {n}")
     station_type = StationType.STATION
-    system = factory.SubFactory(SystemFactory)
+    system = factory.SubFactory(SystemFactoryBase)
     distance_to_arrival = 100
     services = list()
-    controlling_faction = factory.SubFactory(FactionBranchFactory)
+    controlling_faction = factory.SubFactory(FactionBranchFactoryBase)
